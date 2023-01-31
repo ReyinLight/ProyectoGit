a640d75f05328720c061c2927e0699dbe3ea7b81
Reyin <ReyinLight@gmail.com>
2023-01-31 13:35:47 -0600

    Commit random

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,6 +3,6 @@ package org.example;
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("Segunda clase");
     }
 }
