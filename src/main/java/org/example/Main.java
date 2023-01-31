0416e6778b19576f84a1decdfb9c5fa8c1435b3e
ReyinLight <52404859+ReyinLight@users.noreply.github.com>
2023-01-31 15:01:52 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rama del Reyin");
-        System.out.println("Rama del Reyin");
-        System.out.println("Rama del Reyin");
-        System.out.println("Rama del Reyin");
+        System.out.println("Rama principal editada");
     }
 }
