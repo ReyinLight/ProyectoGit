4dc1678edfbbd4bbb77aa382c45984faa564f1f5
JoseLuisReyesPerez <124086822+JoseLuisReyesPerez@users.noreply.github.com>
2023-01-31 15:10:39 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Rama principal editada");
-        System.out.println("Comentario extra");
     }
 }
